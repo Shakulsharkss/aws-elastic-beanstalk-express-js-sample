52cec325fe0e1aa5dc62f3a76fdfb57a30405452
Shakul Hameed <Shakul.Hameed@likewize.com>
2024-08-28 03:31:13 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Shakul World!'));
+app.get('/', (req, res) => res.send('Welcome to Amazing world of Shakul!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
