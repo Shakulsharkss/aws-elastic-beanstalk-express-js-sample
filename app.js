41bb8698dde80c5b8fb14189d4e898ce90d97ee4
Shakul Hameed <Shakul.Hameed@likewize.com>
2024-11-28 02:06:54 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Amazing world of Shakul!'));
+app.get('/', (req, res) => res.send('Welcome to Amazing world of Shakul!. You will be trilled to reach here'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
